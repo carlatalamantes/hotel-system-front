834a43acf401e783c1a3d37722e63cbca286cfec
KeylabMichelle <keylabmichelle@gmail.com>
2022-04-08 22:09:42 -0500

    Room booking, room detail and room catalogue

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,9 @@ import { HeaderComponent } from './layout/header/header.component';
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
 import { SignupComponent } from './pages/signup/signup.component';
+import { RoomsComponent } from './pages/rooms/rooms.component';
+import { BookingComponent } from './pages/booking/booking.component';
+import { RoomdetailComponent } from './pages/roomdetail/roomdetail.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +19,10 @@ import { SignupComponent } from './pages/signup/signup.component';
     HeaderComponent,
     LoginComponent,
     HomeComponent,
-    SignupComponent
+    SignupComponent,
+    RoomsComponent,
+    BookingComponent,
+    RoomdetailComponent
   ],
   imports: [
     BrowserModule,
