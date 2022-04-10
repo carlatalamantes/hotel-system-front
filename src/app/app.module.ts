6b1d7eff71cce59be257f99ce55943b0c626eb60
Carla Talamantes <carlatalamantes@hotmail.com>
2022-04-09 20:23:02 -0500

    Signup connected

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -30,7 +31,8 @@ import { ReactiveFormsModule } from '@angular/forms';
     BrowserModule,
     AppRoutingModule,
     FormsModule, 
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder,  FormGroup, Validators } from '@angular/forms';
+import { SignupService } from 'src/app/services/signup.service';
 
 @Component({
   selector: 'app-signup',
@@ -8,17 +9,15 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 })
 export class SignupComponent implements OnInit {
 
-  signupForm: any;
-  
-  constructor(private fb: FormBuilder) { }
+  signupForm: FormGroup;
+  showError:Boolean=false;
+  showSuccess:Boolean=false;
+  message:String="";
 
-
-  ngOnInit(): void {
-
-     this.signupForm = this.fb.group({
+  constructor(private fb: FormBuilder, private signupService: SignupService) { 
+    this.signupForm = this.fb.group({
       name:['', [Validators.required]],
       first_lastname: ['', [Validators.required]],
-      second_lastname: ['', [Validators.required]],
       cellphone: ['', [
         Validators.required,
         Validators.minLength(10),
@@ -26,14 +25,52 @@ export class SignupComponent implements OnInit {
       ]],
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required]],
+      confirmPassword: ['', [Validators.required]],
+
+    }, {
+      validators: this.matchPasswords.bind(this),
     });
+  }
+
+
+  ngOnInit(): void {
+
+     
 
   }
 
   onSubmit() {
+    if (this.signupForm.valid) {
+      const { name,first_lastname,second_lastname, cellphone, email,password } = this.signupForm.getRawValue();
+      this.signupService.signup({name,first_lastname,second_lastname, cellphone, email,password}).subscribe( {
+        complete: () => {
+          this.showSuccess=true;
+          this.message="User is logged";
+         console.log("Logged")
+        
+    
+        },
+        error: (err) =>{
+          this.showError=true;
+          this.message=err.error.message;
+          console.log(err.error.message)
+        }
+      });
 
-    console.log(this.signupForm.value);
+    } else {
+      console.log(this.signupForm.getRawValue())
+    }
+  }
 
+  //Custom validator
+  matchPasswords() {
+    if (!this.signupForm) return;
+    const { password, confirmPassword } = this.signupForm.getRawValue();
+    if (password === confirmPassword) {
+      return null;
+    } else {
+      return { passwordMismatch: true };
+    }
   }
 
 
