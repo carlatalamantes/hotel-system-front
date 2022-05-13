65c4885ede7401307d4975fb9c6362b7cdc0971f
Carla Talamantes <carlatalamantes@hotmail.com>
2022-05-12 20:10:13 -0500

    url fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   http_timeout: 150, //unit: seconds, 0: no timeout
-  apiUrl: "https://apihotelsystemteamdinamita.herokuapp.com"
+  apiUrl: "https://apihotelsystemteamdinamita.herokuapp.com/"
 };
